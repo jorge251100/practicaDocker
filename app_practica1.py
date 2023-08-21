12454eed578f440483e71f43cafe74112410d217
Jorge Luis <jorge.aragonhe@anahuac.mx>
2023-08-21 14:52:13 -0600

    ADICION

--- app_practica1.py
+++ app_practica1.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return 'App Funcionando!'
 
+@app.route('/holamundo/')
+def holamundo():
+    return 'HOLA MUNDO'
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
